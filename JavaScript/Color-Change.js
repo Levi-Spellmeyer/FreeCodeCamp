51cd23595e479cf3ed582c46518d9d15ca771316
L-Spellmeyer <levi.spellmeyer.04@gmail.com>
2025-08-10 10:40:49 -0500

    Completed excersice from FreeCodeCamps. Involved correcting various syntax errors and no origional coding required

---
+++ JavaScript/Color-Change.js
@@ -0,0 +1,34 @@
+// Note: Not creating a comment block as program was not created by me
+// The extent of this challenge was correcting syntax errors
+// Program is taken from FreeCodeCamps JavaScript excercises
+
+const darkColorsArr = [
+  "#2C3E50",
+  "#34495E",
+  "#2C2C2C",
+  "#616A6B",
+  "#4A235A",
+  "#2F4F4F",
+  "#0E4B5A",
+  "#36454F",
+  "#2C3E50",
+  "#800020",
+];
+
+function getRandomIndex() {
+  const randomIndex = Math.floor(darkColorsArr.length * Math.random());
+  return randomIndex;
+}
+
+const body = document.querySelector("body");
+const bgHexCodeSpanElement = document.querySelector("#bg-hex-code");
+
+function changeBackgroundColor() {
+  const color = darkColorsArr[getRandomIndex()];
+
+  bgHexCodeSpanElement.innerText = color;
+  body.style.backgroundColor = color;
+}
+const btn = document.querySelector("#btn");
+
+btn.onclick = changeBackgroundColor;
